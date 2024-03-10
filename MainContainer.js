6ae7bc3c603b8dd98a5a89621900ec00b685d29f
BHUVANA ATMAKURI <110450831+Bhuvana-Atmakuri@users.noreply.github.com>
2024-03-10 19:44:16 +0530

    Add files via upload

--- MainContainer.js
+++ MainContainer.js
@@ -1,11 +1,14 @@
-
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { View, Text, TouchableOpacity, StyleSheet,TextInput,Button, Alert } from 'react-native';
-import { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { useState, useEffect } from 'react';
 import * as SMS from 'expo-sms';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 // Screens
 import Contacts from './screens/Contacts';
 import Home from './screens/Home';
@@ -20,32 +23,85 @@ const sosAlert = 'SosAlert';
 
 const Tab = createBottomTabNavigator();
 
-
-
 export default function MainContainer() {
-  const [number,setNumber]=useState('6303535448');
-const [message,setMessage]=useState('Yup!This is working');
-// const checkSMS = async() =>{
-//   const isAvailable=await SMS.isAvailableAsync();
-//   if(isAvailable){
-//     alert('SMS is available on this device');
-//   }
-//   else{
-//     alert('SMS is not available on this device');
-//   }
-// };
-const sendSMS = async() =>{
-  const {result} = await SMS.sendSMSAsync(number,message);
-  if(result === 'sent'){
-    alert('Message Sent Successfully');
-  }
-}
-const SosButton = ({ onPress, focused }) => (
-  <TouchableOpacity style={[styles.tabBarButton, focused && styles.sosButton]} onPress={sendSMS}>
-    <Ionicons name="stop-circle" size={30} color={focused ? 'white' : 'red'} />
-    <Text style={[styles.buttonLabel, focused && styles.buttonLabelFocused]}>SOS Alert</Text>
-  </TouchableOpacity>
-);
+  const [message, setMessage] = useState('Yup! This is working');
+  const [familyContacts, setFamilyContacts] = useState([]);
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    const fetchFamilyContacts = async () => {
+      try {
+        const contactsJson = await AsyncStorage.getItem('@familyContacts');
+        if (contactsJson !== null) {
+          const contacts = JSON.parse(contactsJson);
+          setFamilyContacts(contacts);
+        }
+      } catch (error) {
+        console.error('Error retrieving family contacts:', error);
+      }
+    };
+
+    fetchFamilyContacts();
+  }, []);
+
+  useEffect(() => {
+    const userLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+      }
+      let location = await Location.getCurrentPositionAsync({
+        enableHighAccuracy: true,
+      });
+      setMapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    };
+
+    userLocation();
+  }, []);
+
+  const sendSMS = async () => {
+    try {
+      const phoneNumbers = familyContacts.map(contact => contact.phoneNumber);
+
+      // Generate the live location link
+      const locationLink = `https://www.google.com/maps/search/?api=1&query=${mapRegion.latitude},${mapRegion.longitude}`;
+
+      // Construct the message
+      const message = `Emergency: Someone you know or care about needs your help right away! 🚨 Please click this link to see where they are right now: ${locationLink}`;
+
+      const { result } = await SMS.sendSMSAsync(
+        phoneNumbers,
+        message
+      );
+
+      if (result === 'sent') {
+        Alert.alert('Messages Sent Successfully');
+      } else {
+        Alert.alert('Failed to send messages');
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Failed to send messages');
+    }
+  };
+
+  const SosButton = ({ onPress, focused }) => (
+    <TouchableOpacity style={[styles.tabBarButton, focused && styles.sosButton]} onPress={sendSMS}>
+      <Ionicons name="stop-circle" size={30} color={focused ? 'white' : 'red'} />
+      <Text style={[styles.buttonLabel, focused && styles.buttonLabelFocused]}>SOS Alert</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -70,7 +126,6 @@ const SosButton = ({ onPress, focused }) => (
         />
         <Tab.Screen name={contact} component={Contacts} options={{ tabBarIcon: ({ focused }) => <Ionicons name={focused ? 'call' : 'call-outline'} size={25} color="#71C9CE" /> }} />
         <Tab.Screen name={tutorial} component={Tutorials} options={{ tabBarIcon: ({ focused }) => <Ionicons name={focused ? 'logo-youtube' : 'logo-youtube'} size={25} color="#71C9CE" /> }} />
-       
       </Tab.Navigator>
     </NavigationContainer>
   );
